198f003bcc59662fbd3be7047adfe2b32bafa77d
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-10-24 12:00:33 +0300

    save

---
+++ EggCityHunt/App/EggCityHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggCityHuntApp.swift
+//  EggCityHunt
+//
+//  Created by Serhii Babchuk on 24.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggCityHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ECLaunchView()
+        }
+    }
+}
